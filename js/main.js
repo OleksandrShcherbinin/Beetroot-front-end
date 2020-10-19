40bf58d2d41c990e09f1e06049ea73e831de0d41
OleksandrShcherbinin <shcherbininalex81@gmail.com>
2020-10-19 22:11:15 +0300

    totally new template with responsive styles made with prepros

---
+++ js/main.js
@@ -0,0 +1,17 @@
+mybutton = document.getElementById("scroll-mouse");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.opacity = "1";
+  } else {
+    mybutton.style.opacity = "0.3";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
